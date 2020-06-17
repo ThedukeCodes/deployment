388dc2c3737aff9c26d0389313453b08f4f1bb07
Diego Riera <rieradid@tcd.ie>
2020-06-17 21:13:01 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ import main
 SECRET = 'TestSecret'
 TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 EMAIL = 'bigboyplus@outlook.com'
-PASSWORD = 'Navermind55
+PASSWORD = 'Navermind55'
 
 @pytest.fixture
 def client():
